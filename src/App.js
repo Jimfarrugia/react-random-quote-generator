c41b1299c23cd3975e8598d91e98ff8d64346956
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-12 04:52:08 +1100

    add basic layout with html/css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 function App() {
 	return (
-		<div className="App">
-			<p>Hello world</p>
-		</div>
+		<>
+			<div id="quote-box">
+				<blockquote id="quote">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at quam
+					nunc. Vestibulum in quam ut libero fringilla bibendum eget vel justo.
+					Etiam nunc turpis, luctus quis vehicula a, hendrerit sit amet augue.
+				</blockquote>
+				<p id="author">Lorem Ipsum</p>
+				<div id="actions">
+					<a href="#">Twitter</a>
+					<a href="#">Tumblr</a>
+					<a id="new-quote-btn" href="#">
+						New Quote
+					</a>
+					<div class="clear"></div>
+				</div>
+			</div>
+			<footer>
+				by <a href="https://jimfarrugia.com.au">Jim Farrugia</a>
+			</footer>
+		</>
 	);
 }
 
